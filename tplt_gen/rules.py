73fccea9098a7787a90c2f4ed621c1a6169458a4
Kale1d0 <kale1d0@qq.com>
2023-03-08 11:08:26 +0800

    rule: support event trigger

--- tplt_gen/rules.py
+++ tplt_gen/rules.py
@@ -3,71 +3,106 @@ import parse
 from tplt_gen.human import locations_ids
 
 
+def build_transition(trigger: str) -> str:
+    if "?" in trigger:
+        return f'\t\t<label kind="synchronisation">{trigger}</label>\n'
+    else:
+        return f'\t\t<label kind="guard">{trigger}</label>\n'
+
+
+def simple_xml_escape(text: str) -> str:
+    return text.replace("&&", "&amp;&amp;").replace("<", "&lt;").replace(">", "&gt;")
+
+
 def build_rule(name, trigger, anti_trigger, action, delay, offset):
-    trigger = trigger.replace("&&", "&amp;&amp;").replace(
-        "<", "&lt;").replace(">", "&gt;")
-    anti_trigger = anti_trigger.replace("&&", "&amp;&amp;").replace(
-        "<", "&lt;").replace(">", "&gt;")
+    # IF "&&" in trigger:
+    trigger_list = trigger.split("&&") if "&&" in trigger else [trigger]
+    anti_trigger_list = anti_trigger.split(
+        "||") if "||" in anti_trigger else [anti_trigger]
+    assert len(trigger_list) == len(
+        anti_trigger_list), "triggers and anti-triggers does not match"
+    state_triggers = list(filter(lambda x: not "?" in x, trigger_list))
+    event_triggers = list(filter(lambda x: "?" in x, trigger_list))
+    state_anti_triggers = list(
+        filter(lambda x: not "?" in x, anti_trigger_list))
+    event_anti_triggers = list(filter(lambda x: "?" in x, anti_trigger_list))
+    assert len(state_triggers) == len(state_anti_triggers) and len(event_triggers) == len(
+        event_anti_triggers), "triggers and anti-triggers does not match"
+
     vname = name.lower()
+    tplt_l, tplt_t = "", ""
+    tplt_l += f'\t<location id="id{str(offset)}" x="0" y="0">\n'
+    tplt_l += f'\t\t<label kind="invariant">t&lt;={delay}</label>\n'
+    tplt_l += f'\t</location>\n'
+    tplt_l += f'\t<location id="id{str(offset+1)}" x="150" y="0">\n'
+    if len(state_triggers) > 0:
+        tplt_l += f'\t\t<committed/>\n'
+    tplt_l += f'\t</location>\n'
+    tplt_i = f'\t<init ref="id{str(offset)}"/>\n'
+    # Build timer edge
+    tplt_t += f'\t<transition>\n'
+    tplt_t += f'\t\t<source ref="id{str(offset)}"/>\n'
+    tplt_t += f'\t\t<target ref="id{str(offset+1)}"/>\n'
+    tplt_t += f'\t\t<label kind="guard">t&gt;={delay}</label>\n'
+    tplt_t += f'\t\t<label kind="assignment">t=0</label>\n'
+    tplt_t += f'\t</transition>\n'
+    last_node_offset = 1
+    if len(state_triggers) > 0:
+        last_node_offset += 1
+        tplt_l += f'\t<location id="id{str(offset+2)}" x="300" y="0">\n'
+        if len(event_triggers) == 0:
+            tplt_l += f'\t\t<committed/>\n'
+        tplt_l += f'\t</location>\n'
+        # Build state trigger edges
+        trigger_guard = simple_xml_escape("&&".join(state_triggers) if len(
+            state_triggers) > 1 else state_triggers[0])
+        tplt_t += f'\t<transition>\n'
+        tplt_t += f'\t\t<source ref="id{str(offset+1)}"/>\n'
+        tplt_t += f'\t\t<target ref="id{str(offset+2)}"/>\n'
+        tplt_t += f'\t\t<label kind="guard">{trigger_guard}</label>\n'
+        if len(event_triggers) == 0:
+            tplt_t += f'\t\t<label kind="assignment">{vname}=1,t=0</label>\n'
+        tplt_t += f'\t</transition>\n'
+        for i, sat in enumerate(state_anti_triggers):
+            sat = sat.replace(" ", '')
+            tplt_t += f'\t<transition>\n'
+            tplt_t += f'\t\t<source ref="id{str(offset+1)}"/>\n'
+            tplt_t += f'\t\t<target ref="id{str(offset)}"/>\n'
+            tplt_t += f'\t\t<label kind="guard">{simple_xml_escape(sat)}</label>\n'
+            tplt_t += f'\t\t<label kind="assignment">{vname}=0,t=0</label>\n'
+            tplt_t += f'\t\t<nail x="150" y="{(i+1)*50}"/>\n'
+            tplt_t += f'\t\t<nail x="0" y="{(i+1)*50}"/>\n'
+            tplt_t += f'\t</transition>\n'
+
+    if len(event_triggers) > 0:
+        # Build event trigger edges
+        for i, event_trigger in enumerate(event_triggers):
+            last_node_offset += 1
+            tplt_l += f'\t<location id="id{str(offset+last_node_offset)}" x="{last_node_offset * 150}" y="0">\n'
+            tplt_l += f'\t</location>\n'
+            tplt_t += f'\t<transition>\n'
+            tplt_t += f'\t\t<source ref="id{str(offset+last_node_offset-1)}"/>\n'
+            tplt_t += f'\t\t<target ref="id{str(offset+last_node_offset)}"/>\n'
+            tplt_t += f'\t\t<label kind="synchronisation">{simple_xml_escape(event_trigger)}</label>\n'
+            # Mark the rule as enabled only if the last trigger is fulfilled
+            if i == len(event_triggers) - 1:
+                tplt_t += f'\t\t<label kind="assignment">{vname}=1,t=0</label>\n'
+            tplt_t += f'\t</transition>\n'
+
+    # Build action edge
+    tplt_t += f'\t<transition>\n'
+    tplt_t += f'\t\t<source ref="id{str(offset+last_node_offset)}"/>\n'
+    tplt_t += f'\t\t<target ref="id{str(offset)}"/>\n'
+    tplt_t += f'\t\t<label kind="synchronisation">{action}</label>\n'
+    tplt_t += f'\t\t<nail x="{last_node_offset * 150}" y="-50"/>\n'
+    tplt_t += f'\t\t<nail x="0" y="-50"/>\n'
+    tplt_t += f'\t</transition>\n'
+
     template = ""
     template += f'<template>\n'
     template += f'\t<name>{name}</name>\n'
     template += f'\t<declaration>clock t;</declaration>\n'
-    template += f'\t<location id="id{str(offset)}" x="0" y="0">\n'
-    template += f'\t\t<label kind="invariant">t&lt;={delay}</label>\n'
-    template += f'\t</location>\n'
-    template += f'\t<location id="id{str(offset+1)}" x="150" y="0">\n'
-    template += f'\t\t<committed/>\n'
-    template += f'\t</location>\n'
-    template += f'\t<location id="id{str(offset+2)}" x="300" y="0">\n'
-    template += f'\t\t<committed/>\n'
-    template += f'\t</location>\n'
-
-    template += f'\t<init ref="id{str(offset)}"/>\n'
-
-    template += f'\t<transition>\n'
-    template += f'\t\t<source ref="id{str(offset)}"/>\n'
-    template += f'\t\t<target ref="id{str(offset+1)}"/>\n'
-    template += f'\t\t<label kind="guard">t&gt;={delay}</label>\n'
-    template += f'\t\t<label kind="assignment">t=0</label>\n'
-    template += f'\t</transition>\n'
-
-    template += f'\t<transition>\n'
-    template += f'\t\t<source ref="id{str(offset+1)}"/>\n'
-    template += f'\t\t<target ref="id{str(offset+2)}"/>\n'
-    template += f'\t\t<label kind="guard">{trigger}</label>\n'
-    template += f'\t\t<label kind="assignment">{vname}=1,t=0</label>\n'
-    template += f'\t</transition>\n'
-
-    template += f'\t<transition>\n'
-    template += f'\t\t<source ref="id{str(offset+2)}"/>\n'
-    template += f'\t\t<target ref="id{str(offset)}"/>\n'
-    template += f'\t\t<label kind="synchronisation">{action}</label>\n'
-    template += f'\t\t<nail x="300" y="-50"/>\n'
-    template += f'\t\t<nail x="0" y="-50"/>\n'
-    template += f'\t</transition>\n'
-    if "||" in anti_trigger:
-        sub_anti_triggers = anti_trigger.split("||")
-        for sat in sub_anti_triggers:
-            sat = sat.replace(" ", '')
-            template += f'\t<transition>\n'
-            template += f'\t\t<source ref="id{str(offset+1)}"/>\n'
-            template += f'\t\t<target ref="id{str(offset)}"/>\n'
-            template += f'\t\t<label kind="guard">{sat}</label>\n'
-            template += f'\t\t<label kind="assignment">{vname}=0,t=0</label>\n'
-            template += f'\t\t<nail x="150" y="50"/>\n'
-            template += f'\t\t<nail x="0" y="50"/>\n'
-            template += f'\t</transition>\n'
-    else:
-        template += f'\t<transition>\n'
-        template += f'\t\t<source ref="id{str(offset+1)}"/>\n'
-        template += f'\t\t<target ref="id{str(offset)}"/>\n'
-        template += f'\t\t<label kind="guard">{anti_trigger}</label>\n'
-        template += f'\t\t<label kind="assignment">{vname}=0,t=0</label>\n'
-        template += f'\t\t<nail x="150" y="50"/>\n'
-        template += f'\t\t<nail x="0" y="50"/>\n'
-        template += f'\t</transition>\n'
-
+    template += tplt_l + tplt_i + tplt_t
     template += f'</template>\n'
     return template
 
@@ -79,6 +114,10 @@ trigger_mappings = {
     '{name}_{i}.off': '{name}[{i}]==0',
     '{name}_{i}.open': '{name}[{i}]==1',
     '{name}_{i}.closed': '{name}[{i}]==0',
+    '{name}_{i}.turn_{name}_on': 'turn_on_{name}[{i}]?',
+    '{name}_{i}.turn_{name}_off': 'turn_off_{name}[{i}]?',
+    '{name}_{i}.open_{name}': 'open_{name}[{i}]?',
+    '{name}_{i}.close_{name}': 'close_{name}[{i}]?',
 }
 
 comparisons = ["==", "!=", "<", ">", "<=", ">="]
@@ -139,8 +178,8 @@ def parse_tap_rules(text: str):
         trigger = line[line.find("IF ") + len("IF "):line.find(" THEN ")]
         action = line[line.find(" THEN ") + len(" THEN "):]
         inner_t, inner_a = parse_rule(trigger, action)
-        assert inner_t != None, f"bad trigger: {trigger}"
-        assert inner_a != None, f"bad action: {action}"
+        assert inner_t != None, f"bad trigger: {trigger}, in {text}"
+        assert inner_a != None, f"bad action: {action}, in {text}"
         triggers.append(inner_t)
         actions.append(inner_a)
     return triggers, actions
@@ -155,6 +194,8 @@ def to_anti_trigger(trigger: str) -> str:
     if "&&" in trigger:
         sub_triggers = trigger.split("&&")
         return "||".join(list(map(to_anti_trigger, sub_triggers)))
+    elif "?" in trigger:
+        return trigger  # DO NOT EDIT EVENT TRIGGER
     else:
         for pair in anti_pairs:
             f, s = pair
@@ -173,8 +214,9 @@ assert to_anti_trigger("a>b&&a<=b") == "a<=b||a>b"
 
 if __name__ == "__main__":
     rule_delay = 1
-    tap_sets = ['RQ3Case1', 'RQ3Case3', 'RQ3Case4',
-                'RQ3Case5', 'RQ3Case6', 'RQ3Case7', 'RQ3Case9']
+    # tap_sets = ['RQ3Case1', 'RQ3Case3', 'RQ3Case4',
+    #             'RQ3Case5', 'RQ3Case6', 'RQ3Case7', 'RQ3Case9', 'RQ3Case10']
+    tap_sets = ["test_state_event"]
     for tap_set in tap_sets:
         if not os.path.exists(f'rule_templates/{tap_set}'):
             os.mkdir(f'rule_templates/{tap_set}')
