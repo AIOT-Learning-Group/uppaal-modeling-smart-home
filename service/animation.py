daee845ea6a59f61ce5024162a1e9be0b708d1fb
Kale1d0 <kale1d0@qq.com>
2023-04-23 18:46:51 +0800

    add animation service router

---
+++ service/animation.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/api/generate-animation-specification")
+async def generate_animation_specification() -> None:
+    # TODO: implement this function
+    pass
