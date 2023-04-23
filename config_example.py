547f31effd2bd0cc855d0466cc7a05587a39d069
Kale1d0 <kale1d0@qq.com>
2023-04-23 18:09:50 +0800

    use port from config

--- config_example.py
+++ config_example.py
@@ -1,3 +1,4 @@
 POINTS_NUMBER: int = 500
 RULE_DELAY: float = 1.0
 UPPAAL_PATH: str = "D:\\Workspace\\TapSim\\uppaal-4.1.24\\bin-Windows"
+SERVER_PORT: int = 8000
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from service.simulation import router as sim_router
 from service.resource import router as res_router
 from service.synthesization import router as syn_router
+from config import SERVER_PORT
 
 app = FastAPI()
 app.include_router(res_router)
@@ -12,4 +13,4 @@ app.include_router(sim_router)
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=SERVER_PORT)
