f7ecef1b3d84dd3883d0d821510e0d79d9f2eb39
Kale1d0 <kale1d0@qq.com>
2023-04-26 20:09:16 +0800

    rename service.animation to service.rendering

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from fastapi.staticfiles import StaticFiles
 from service.simulation import router as sim_router
 from service.resource import router as res_router
 from service.synthesization import router as syn_router
-from service.animation import router as ani_router
+from service.rendering import router as ren_router
 from config import SERVER_PORT
 
 app = FastAPI()
 app.include_router(res_router)
 app.include_router(syn_router)
 app.include_router(sim_router)
-app.include_router(ani_router)
+app.include_router(ren_router)
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == "__main__":
